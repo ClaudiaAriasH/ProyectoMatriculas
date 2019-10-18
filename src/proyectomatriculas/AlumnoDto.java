106d74387ce52448c458e7ae35bede88edb29726
Henry <>
2019-10-17 20:34:54 -0500

    Se adiciona la clase AlumnoDto

---
+++ src/proyectomatriculas/AlumnoDto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectomatriculas;
+
+/**
+ *
+ * @author hmunoze
+ */
+public class AlumnoDto extends DatosPersonales{
+ 
+    private boolean becado;
+
+    public boolean isBecado() {
+        return becado;
+    }
+
+    public void setBecado(boolean becado) {
+        this.becado = becado;
+    }
+
+    @Override
+    public String toString() {
+        return "becado=" + becado;
+    }
+}
